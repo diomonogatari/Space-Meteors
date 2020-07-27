1e16b8de9b76ab9c98fb43d2bfb5bd661943a099
Diogo Rosa Carvalho <diodude.lol@gmail.com>
2020-07-27 21:46:53 +0100

    Just title text and some scripts

---
+++ Space Meteors/Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject player; //knowing who the player is is nice. allows me to call his actions :)
+    //I don't know who the enemies will be because there will be a lot of them
+    
+    //The GameController will not know who the enemies are, but the enemies, as they trigger lose conditions, will be able to communicate with the GameController
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
